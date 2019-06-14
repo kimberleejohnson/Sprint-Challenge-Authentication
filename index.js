fcba2986acfed7e465542de5d74a5063bb2b6265
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-06-14 08:25:52 -0700

    Added comments and also installed express, cors, and helmet dependencies

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config(); // load .env variables
 
+// Requiring my server
 const server = require('./api/server.js');
 
+// Telling my server to listen on port 3300
 const port = process.env.PORT || 3300;
 server.listen(port, () => {
   console.log(`\n=== Server listening on port ${port}\n`);
