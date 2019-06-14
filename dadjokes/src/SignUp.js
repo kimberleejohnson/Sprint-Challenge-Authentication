0f764877a6dc0810438649e1fde4ad1f88057130
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-06-14 09:37:54 -0700

    installed axios locally and built my axios helper to import into my routes and make my code more dry

---
+++ dadjokes/src/SignUp.js
@@ -0,0 +1,7 @@
+//This route will let a new user sign up to view dadjokes 
+
+import React from 'react'; 
+
+// Eventually I'll need to import withRouter to redirect on register
+
+// Importing axios helper, so my code can be more dry
---
+++ dadjokes/src/helpers/api.js
@@ -0,0 +1,21 @@
+// Importing axios so I can make HTTP requests
+// Creating an axios instance to make code more dry
+import axios from 'axios'; 
+
+const instance = axios.create({
+    baseURL: 'http://localhost:5500/api'
+})
+
+// Setting up an interceptor to run the page multiple times to avoid my 401 error 
+// Will run every time a request is made 
+instance.interceptors.request.use(
+    (config) => {
+        config.headers.authorization = localStorage.getItem('token'); 
+        return config;
+    },
+    (err) => {
+        return Promise.reject(err); 
+    }
+)
+
+export default instance; 
